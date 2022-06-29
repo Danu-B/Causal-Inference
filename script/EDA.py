7f4a870cd7167ecbcb1ac7db6b5ce2aeae067d7c
unknown <dbulom12@gmail.com>
2022-06-29 23:52:57 +0300

    Data exploration script

--- script/EDA.py
+++ script/EDA.py
@@ -1,134 +1,135 @@
 import numpy as np
 import pandas as pd
 
-def drop_duplicates(df):
-    old = df.shape[0]
-    df.drop_duplicates(inplace=True)
-    new = df.shape[0]
-    count = old - new
-    if (count == 0):
-        print("No duplicate rows were found.")
-    else:
-        print(f"{count} duplicate rows were found and removed.")
+Class EDA
+    def drop_duplicates(df):
+        old = df.shape[0]
+        df.drop_duplicates(inplace=True)
+        new = df.shape[0]
+        count = old - new
+        if (count == 0):
+            print("No duplicate rows were found.")
+        else:
+            print(f"{count} duplicate rows were found and removed.")
 
-def show_cols_mixed_dtypes(df):
-    mixed_dtypes = {'Column': [], 'Data type': []}
-    for col in df.columns:
-        dtype = pd.api.types.infer_dtype(df[col])
+    def show_cols_mixed_dtypes(df):
+        mixed_dtypes = {'Column': [], 'Data type': []}
+        for col in df.columns:
+            dtype = pd.api.types.infer_dtype(df[col])
         if dtype.startswith("mixed"):
             mixed_dtypes['Column'].append(col)
             mixed_dtypes['Data type'].append(dtype)
-    if len(mixed_dtypes['Column']) == 0:
-        print('None of the columns contain mixed types.')
-    else:
-        print(pd.DataFrame(mixed_dtypes))
+        if len(mixed_dtypes['Column']) == 0:
+            print('None of the columns contain mixed types.')
+        else:
+            print(pd.DataFrame(mixed_dtypes))
 
-def percent_missing_values(df):
+    def percent_missing_values(df):
 
-    # Calculate total number of cells in dataframe
-    totalCells = np.product(df.shape)
+        # Calculate total number of cells in dataframe
+        totalCells = np.product(df.shape)
 
-    # Count number of missing values per column
-    missingCount = df.isnull().sum()
+        # Count number of missing values per column
+        missingCount = df.isnull().sum()
 
-    # Calculate total number of missing values
-    totalMissing = missingCount.sum()
+        # Calculate total number of missing values
+        totalMissing = missingCount.sum()
 
-    # Calculate percentage of missing values
-    print("The dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
+        # Calculate percentage of missing values
+        print("The dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
 
-def count_missing_rows(df):
+    def count_missing_rows(df):
 
-    # Calculate total number rows with missing values
-    missing_rows = sum([True for idx,row in df.iterrows() if any(row.isna())])
+        # Calculate total number rows with missing values
+        missing_rows = sum([True for idx,row in df.iterrows() if any(row.isna())])
 
-    # Calculate total number of rows
-    total_rows = df.shape[0]
+        # Calculate total number of rows
+        total_rows = df.shape[0]
 
-    # Calculate the percentage of missing rows
-    print(f"{missing_rows} rows({round(((missing_rows/total_rows) * 100), 2)}%) contain atleast one missing value.")
+        # Calculate the percentage of missing rows
+        print(f"{missing_rows} rows({round(((missing_rows/total_rows) * 100), 2)}%) contain atleast one missing value.")
 
-# Function to calculate missing values by column
-def missing_values_table(df):
-    # Total missing values
-    mis_val = df.isnull().sum()
+    # Function to calculate missing values by column
+    def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
 
-    # Percentage of missing values
-    mis_val_percent = 100 * mis_val / len(df)
+        # Percentage of missing values
+        mis_val_percent = 100 * mis_val / len(df)
 
-    # dtype of missing values
-    mis_val_dtype = df.dtypes
+        # dtype of missing values
+        mis_val_dtype = df.dtypes
 
-    # Make a table with the results
-    mis_val_table = pd.concat([mis_val, mis_val_percent, mis_val_dtype], axis=1)
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent, mis_val_dtype], axis=1)
 
-    # Rename the columns
-    mis_val_table_ren_columns = mis_val_table.rename(
-    columns = {0 : 'Missing Values', 1 : '% of Total Values', 2: 'Dtype'})
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values', 2: 'Dtype'})
 
-    # Sort the table by percentage of missing descending and remove columns with no missing values
-    mis_val_table_ren_columns = mis_val_table_ren_columns[
-        mis_val_table_ren_columns.iloc[:,0] != 0].sort_values(
+        # Sort the table by percentage of missing descending and remove columns with no missing values
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,0] != 0].sort_values(
     '% of Total Values', ascending=False).round(2)
 
-    # Print some summary information
-    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
         "There are " + str(mis_val_table_ren_columns.shape[0]) +
           " columns that have missing values.")
 
-    if mis_val_table_ren_columns.shape[0] == 0:
-        return
+        if mis_val_table_ren_columns.shape[0] == 0:
+            return
 
-    # Return the dataframe with missing information
-    return mis_val_table_ren_columns
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
 
 
-def fix_missing_ffill_bfill(df, cols):
-    for col in cols:
-        count = df[col].isna().sum()
-        df[col] = df[col].fillna(method='ffill')
-        df[col] = df[col].fillna(method='bfill')
-        print(f"{count} missing values in the column {col} have been replaced \
+    def fix_missing_ffill_bfill(df, cols):
+        for col in cols:
+            count = df[col].isna().sum()
+            df[col] = df[col].fillna(method='ffill')
+            df[col] = df[col].fillna(method='bfill')
+            print(f"{count} missing values in the column {col} have been replaced \
             first by the forward fill then by the backward fill methods.")
 
-def fix_missing_median(df, cols):
-    for col in cols:
-        median = df[col].median()
-        count = df[col].isna().sum()
-        df[col] = df[col].fillna(median)
-        print(f"{count} missing values in the column {col} have been replaced by its median value {median}.")
+    def fix_missing_median(df, cols):
+        for col in cols:
+            median = df[col].median()
+            count = df[col].isna().sum()
+            df[col] = df[col].fillna(median)
+            print(f"{count} missing values in the column {col} have been replaced by its median value {median}.")
 
-def fix_missing_mode(df, cols):
-    for col in cols:
-        mode = df[col].mode()[0]
-        count = df[col].isna().sum()
-        df[col] = df[col].fillna(mode)
-        if type(mode) == 'str':
-            print(f"{count} missing values in the column {col} have been replaced by its mode value \'{mode}\'.")
-        else:
-            print(f"{count} missing values in the column {col} have been replaced by its mode value {mode}.")
+    def fix_missing_mode(df, cols):
+        for col in cols:
+            mode = df[col].mode()[0]
+            count = df[col].isna().sum()
+            df[col] = df[col].fillna(mode)
+            if type(mode) == 'str':
+                print(f"{count} missing values in the column {col} have been replaced by its mode value \'{mode}\'.")
+            else:
+                print(f"{count} missing values in the column {col} have been replaced by its mode value {mode}.")
 
-def fix_missing_value(df, cols, value):
-    for col in cols:
-        count = df[col].isna().sum()
-        df[col] = df[col].fillna(value)
-        if type(value) == 'str':
+    def fix_missing_value(df, cols, value):
+        for col in cols:
+            count = df[col].isna().sum()
+            df[col] = df[col].fillna(value)
+            if type(value) == 'str':
             print(f"{count} missing values in the column {col} have been replaced by \'{value}\'.")
+            else:
+                print(f"{count} missing values in the column {col} have been replaced by {value}.")
+
+
+    def drop_rows_with_missing_values(df):
+        old = df.shape[0]
+        df.dropna(inplace=True)
+        new = df.shape[0]
+        count = old - new
+        print(f"{count} rows containg missing values were dropped.")
+
+    def drop_columns(df, columns):
+        df.drop(columns, axis=1, inplace=True)
+        count = len(columns)
+        if count == 1:
+            print(f"{count} column was dropped.")
         else:
-            print(f"{count} missing values in the column {col} have been replaced by {value}.")
-
-
-def drop_rows_with_missing_values(df):
-    old = df.shape[0]
-    df.dropna(inplace=True)
-    new = df.shape[0]
-    count = old - new
-    print(f"{count} rows containg missing values were dropped.")
-
-def drop_columns(df, columns):
-    df.drop(columns, axis=1, inplace=True)
-    count = len(columns)
-    if count == 1:
-        print(f"{count} column was dropped.")
-    else:
-        print(f"{count} columns were dropped.")
+            print(f"{count} columns were dropped.")
---
+++ script/plot.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
+class Plot:
+    def plotly_plot_pie(df, column, limit=None, title=None):
+        a = pd.DataFrame({'count': df.groupby([column]).size()}).reset_index()
+        a = a.sort_values("count", ascending=False)
+        if limit:
+            a.loc[a['count'] < limit, column] = f'Other {column}s'
+        if title == None:
+            title=f'Distribution of {column}'
+        fig = px.pie(a, values='count', names=column, title=title, width=800, height=500)
+            fig.show()
+
+    def plot_hist(df:pd.DataFrame, column:str, color:str='cornflowerblue')->None:
+        sns.displot(data=df, x=column, color=color, kde=True, height=6, aspect=2)
+        plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+        plt.show()
+
+    def plot_count(df:pd.DataFrame, column:str) -> None:
+        plt.figure(figsize=(12, 7))
+        sns.countplot(data=df, x=column)
+        plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+        plt.show()
+    
+    
+    def plotly_plot_scatter(df, x_col, y_col, marker_size, hover=[]):
+        fig = px.scatter(
+            df,
+            x=x_col,
+            y=y_col,
+            opacity=0.8,
+            hover_data=hover,
+            title=f'{x_col} vs. {y_col}')
+        fig.update_traces(marker_size=marker_size)
+        fig.show()
+    
